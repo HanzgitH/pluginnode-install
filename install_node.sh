d4d6abae97e58ec57889addbfcc6f1dad3f8a9a0
Hanz (@DeFi_Jon) <98694477+HanzgitH@users.noreply.github.com>
2022-12-15 19:25:32 +0000

    Update install_node.sh

--- install_node.sh
+++ install_node.sh
@@ -181,8 +181,8 @@ echo
 echo -e "Done..."
 
 echo -e "\n\n############ Updating RPC and WS information  ##########\n\n" && sleep 5
-sudo sed -i -e "s/plirpc.blocksscan.io/pliws.xdcrpc.com/g" /opt/docker/goplugin/plugin-deployment/startEI.sh
-sudo sed -i -e "s/pluginws.blocksscan.io/pliws1.xdcrpc.com/g" /opt/docker/goplugin/plugin-deployment/plugin.env
+sudo sed -i -e "s/plirpc.blocksscan.io/plixdcrpc.icotokens.net/g" /opt/docker/goplugin/plugin-deployment/startEI.sh
+sudo sed -i -e "s/pluginws.blocksscan.io/plixdcwss.icotokens.net/g" /opt/docker/goplugin/plugin-deployment/plugin.env
 wait
 
 echo -e "\n\n################# Bringing up node & database #################\n\n"
